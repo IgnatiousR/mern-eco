2080c61780172a551d640be6adc75ec7bf402198
Ignatious Rozario <ignatiousr80@gmail.com>
2025-12-09 21:46:06 +0600

    Update NewArrivals.tsx

--- frontend/src/components/Products/NewArrivals.tsx
+++ frontend/src/components/Products/NewArrivals.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { useEffect, useRef, useState } from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { Link } from "react-router";
 
 const NewArrivals = () => {
+  const scrollRef = useRef<HTMLDivElement>(null);
+  const [isDragging, setIsDragging] = useState(false);
+  const [startX, setStartX] = useState(0);
+  const [scrollLeft, setScrollLeft] = useState(false);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
   const newArrivals = [
     {
       _id: "1",
@@ -12,6 +20,7 @@ const NewArrivals = () => {
           altText: "Prod 1",
         },
       ],
+      price: "10.99",
     },
     {
       _id: "2",
@@ -22,6 +31,7 @@ const NewArrivals = () => {
           altText: "Prod 2",
         },
       ],
+      price: "20.99",
     },
     {
       _id: "3",
@@ -32,6 +42,7 @@ const NewArrivals = () => {
           altText: "Prod 3",
         },
       ],
+      price: "2.99",
     },
     {
       _id: "4",
@@ -42,6 +53,7 @@ const NewArrivals = () => {
           altText: "Prod 4",
         },
       ],
+      price: "7.99",
     },
     {
       _id: "5",
@@ -52,6 +64,7 @@ const NewArrivals = () => {
           altText: "Prod 5",
         },
       ],
+      price: "120.99",
     },
     {
       _id: "6",
@@ -62,6 +75,7 @@ const NewArrivals = () => {
           altText: "Prod 6",
         },
       ],
+      price: "40.99",
     },
     {
       _id: "7",
@@ -72,6 +86,7 @@ const NewArrivals = () => {
           altText: "Prod 7",
         },
       ],
+      price: "80.99",
     },
     {
       _id: "8",
@@ -82,22 +97,70 @@ const NewArrivals = () => {
           altText: "Prod 8",
         },
       ],
+      price: "87.99",
     },
   ];
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true);
+    if (scrollRef.current == null) return;
+    setStartX(e.pageX - scrollRef.current.offsetLeft);
+  };
+
+  const scroll = (direction) => {
+    const scrollAmount = direction === "left" ? -300 : 300;
+    scrollRef.current.scrollBy({ left: scrollAmount, behavior: "smooth" });
+  };
+
+  //Update Scroll Buttons State
+  const updateScrollButtons = () => {
+    const container = scrollRef.current;
+    console.log({
+      scrollLeft: container?.scrollLeft,
+      clientWidth: container?.clientWidth,
+      containerScrollWidth: container?.scrollWidth,
+      offsetLeft: scrollRef.current?.offsetLeft,
+    });
+
+    if (container) {
+      const leftScroll = container.scrollLeft;
+      const rightScrollable = container.scrollWidth > leftScroll + container.clientWidth;
+      setCanScrollLeft(leftScroll > 0);
+      setCanScrollRight(rightScrollable);
+    }
+  };
+
+  useEffect(() => {
+    const container = scrollRef.current;
+    if (container) {
+      container.addEventListener("scroll", updateScrollButtons);
+    }
+  }, []);
+
   return (
     <section>
       <div className="container mx-auto text-center mb-10 relative">
         <h2 className="text-3xl font-bold mb-4">Explore New Arrivals</h2>
-        <p className="text-lg text-zinc-600 mb-8">
-          Check our latest products, freshly arrived to our store.
-        </p>
+        <p className="text-lg text-zinc-600 mb-8">Check our latest products, freshly arrived to our store.</p>
 
         {/* Scroll Buttons */}
         <div className="absolute right-0 bottom-[-30px] flex space-x-2">
-          <button className="p-2 rounded border border-zinc-300 bg-white text-black">
+          <button
+            onClick={() => scroll("left")}
+            disabled={!canScrollLeft}
+            className={`p-2 rounded border ${
+              canScrollLeft ? "bg-white text-black" : "bg-zinc-100 text-gray-400 cursor-not-allowed"
+            } border-zinc-300`}
+          >
             <FiChevronLeft className="text-2xl" />
           </button>
-          <button className="p-2 rounded border border-zinc-300 bg-white text-black">
+          <button
+            onClick={() => scroll("right")}
+            disabled={!canScrollRight}
+            className={`p-2 rounded border ${
+              canScrollRight ? "bg-white text-black" : "bg-zinc-100 text-gray-400 cursor-not-allowed"
+            } border-zinc-300`}
+          >
             <FiChevronRight className="text-2xl" />
           </button>
         </div>
@@ -105,12 +168,27 @@ const NewArrivals = () => {
 
       {/* Scrollable content */}
       <div
+        ref={scrollRef}
         className="container mx-auto scrollbar overflow-x-scroll flex space-x-6 
-      relative scrollbar-track-zinc-200 crollbar-track-rounded-full scrollbar-thumb-sky-700"
+      relative scrollbar-track-zinc-200 crollbar-track-rounded-full scrollbar-thumb-zinc-700"
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUpOrLeave}
+        onMouseLeave={handleMouseUpOrLeave}
       >
         {newArrivals.map((product) => (
-          <div key={product._id}>
-            <img src={product.images[0].url} alt={product.images[0]?.altText || product.name} />
+          <div key={product._id} className="min-w-full sm:min-w-[50%] lg:min-w-[30%] relative">
+            <img
+              src={product.images[0].url}
+              alt={product.images[0]?.altText || product.name}
+              className="w-full h-[500px] object-cover rounded-lg"
+            />
+            <div className="absolute bottom-0 left-0 right-0 bg-opacity-50 backdrop-blur-md text-white p-4 rounded-b-lg">
+              <Link to={`/products/${product._id}`} className="block">
+                <h4 className="font-medium">{product.name}</h4>
+                <p className="mt-1">{product.price}</p>
+              </Link>
+            </div>
           </div>
         ))}
       </div>
