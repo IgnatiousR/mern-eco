3aa43d6bdc079e79fbce8854380a7cbcb9a1af69
Ignatious <ignatiousr80@gmail.com>
2025-12-15 10:13:48 +0600

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import UserLayout from "./components/Layouts/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
   return (
@@ -10,7 +12,9 @@ const App = () => {
       <Routes>
         <Route path="/" element={<UserLayout />}>
           {/* User Layout */}
-          <Route index element={<Home />}></Route>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
         </Route>
         <Route>{/* Admin Layout */}</Route>
       </Routes>
