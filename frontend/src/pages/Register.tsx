04fb333d14a9195afbaaed1fbd0fa96713665a4a
Ignatious <ignatiousr80@gmail.com>
2025-12-15 10:13:46 +0600

    Create Register.tsx

---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,105 @@
+import { useState } from "react";
+import { Link } from "react-router";
+import registerImg from "../assets/register.webp";
+import { toast } from "sonner";
+
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!name.trim() || !email.trim() || !password || !confirmPassword) {
+      toast.error("Please fill all the inputs.");
+      return;
+    }
+    if (password != confirmPassword) {
+      toast.error("Password didnot match.");
+      return;
+    }
+    console.log("User Registered:", { name, email, password, confirmPassword });
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-full md:w-1/2 flex flex-col justify-center items-center p-8 md:p-12">
+        <form
+          onSubmit={handleSubmit}
+          className="w-full max-w-md bg-white p-8 rounded-lg border shadow-sm"
+        >
+          <div className="flex justify-center mb-6">
+            <h2 className="text-xl font-medium">Rabbit</h2>
+          </div>
+          <h2 className="text-2xl font-bold text-center mb-6">Hey there! 👋</h2>
+          <p className="text-center mb-6">Enter your username and password to Login</p>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Name</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your name"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Email</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Enter your password"
+            />
+          </div>
+          <div className="mb-4">
+            <label className="block text-sm font-semibold mb-2">Confirm Password</label>
+            <input
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              className="w-full p-2 border rounded"
+              placeholder="Confirm your password"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full bg-black text-white p-2 rounded-lg font-semibold hover:bg-zinc-800 transition"
+          >
+            Sign Up
+          </button>
+          <p className="mt-6 text-center text-sm">
+            Already have an account?{" "}
+            <Link to="/login" className="text-red-500">
+              Login
+            </Link>
+          </p>
+        </form>
+      </div>
+
+      <div className="hidden md:block w-1/2 bg-zinc-800">
+        <div className="h-full flex flex-col justify-center items-center">
+          <img
+            src={registerImg}
+            alt="Register an Account"
+            className="h-[750px] w-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
